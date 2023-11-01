04f3655f93c931a0c7789da3e795ec5a48345c49
faysalhosen <faysanhosen7025@gmail.com>
2023-11-01 20:58:37 +0600

    jwt token verified

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 5001;
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
 
-//middleware
-app.use(cors());
-app.use(express.json());
-
+// middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173/"],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+
+//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.hf0b3tt.mongodb.net/?retryWrites=true&w=majority`;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wzxk65v.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,27 +27,26 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 
-// middlewares
+// middlewares related to JWT
 const verifyToken = (req, res, next)=>{
   const token = req.cookies?.token;
-  console.log("value of token in middleware", token)
   if(!token){
-      return res.status(401).send({message: "unauthorized"})
+    return res.status(401).send({message: "unauthorized"});
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=>{
-      //error
-      if(error){
-          return res.status(401),send({message: "unauthorized"})
-      }
-      // if token is valid then it would be decoded
-      req.user = decoded;
-      next()
+    //error
+    if(error){
+      return res.status(401).send({message: "unauthorized"})
+    }
+    //if token is valid then only it would be decoded
+    req.user = decoded;
+   
+    next()
   })
-
 }
 
 async function run() {
@@ -55,96 +55,101 @@ async function run() {
     await client.connect();
 
     // service collection
-    const servicesCollection = client.db("carDoctorDB").collection("services")
+    const servicesCollection = client.db("carDoctorDB").collection("services");
     // order collection
-    const ordersCollection = client.db("carDoctorDB").collection("orders")
+    const ordersCollection = client.db("carDoctorDB").collection("orders");
 
-        // auth related api
-        app.post("/jwt", async (req, res) => {
-          const user = req.body;
-          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-            expiresIn: '1h',
-          });
-    
-          res
-          .cookie('token', token, {
-            httpOnly:true,
-            secure:false,
-    
-          })
-          .send({success:true})
-    
-        });
+    // auth related api
+    app.post("/jwt", (req,res)=>{
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h"
+      })
+       res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true
+        })
+        .send({success:true})
+    })
+    // clear cookie api, JWT
+    app.post("/logout", (req,res)=>{
+      const user = req.body;
+      console.log(user)
+      res.clearCookie("token", {maxAge:0}).send({success:true})
+    })
 
     // get services endpoint
-    app.get("/services", async(req, res)=>{
-        const cursor = servicesCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     // get a single service
-    app.get("/services/:id", async(req, res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await servicesCollection.findOne(query)
-        res.send(result)
-    })
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
+      res.send(result);
+    });
 
     // get endpoint of orders
-    app.get("/orders", async(req, res)=>{
-        let query = {};
-        if(req.query?.email){
-            query = {email: req.query.email}
-        }
-        const result = await ordersCollection.find(query).toArray()
-        res.send(result)
-    })
+    app.get("/orders", verifyToken, async (req, res) => {
+      if(req.query?.email !== req.user?.email){
+        return res.status(403).send({message: "forbidden"})
+      }
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await ordersCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // post a order by user, POST endpoint
-    app.post("/orders", async(req, res)=>{
-        const order = req.body;
-        const result = await ordersCollection.insertOne(order)
-        res.send(result)
-    })
-      // order, delete endpoint
-      app.delete("/orders/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await ordersCollection.deleteOne(query);
-        res.send(result);
-      });
-       // order, patch endpoint
-    app.patch("/orders/:id", async(req, res)=>{
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      res.send(result);
+    });
+    // order, delete endpoint
+    app.delete("/orders/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
-      const updateOrder = req.body
+      const query = { _id: new ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // order, patch endpoint
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateOrder = req.body;
       const updatedDoc = {
-          $set: {
-              status: updateOrder.status
-          }
-      }
-      const result = await ordersCollection.updateOne(filter, updatedDoc)
-      res.send(result)
-  })
-      
-
-
+        $set: {
+          status: updateOrder.status,
+        },
+      };
+      const result = await ordersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    //await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Car doctor server is RUNNING!");
+});
 
-app.get('/', (req,res) => {
-    res.send('doctor is running')
-})
-
-app.listen(port,() => {
-    console.log(`car Doctor Server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
